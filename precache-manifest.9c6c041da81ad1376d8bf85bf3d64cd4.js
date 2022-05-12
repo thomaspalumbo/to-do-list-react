6d4f343cc6b3256c184f65f50943aa0c15404b30
thomaspalumbo <thomaspalumbomazek@gmail.com>
2022-05-12 11:27:31 -0300

    Updates

---
+++ precache-manifest.9c6c041da81ad1376d8bf85bf3d64cd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50a9c6cfe2903e5ff895",
+    "url": "/to-do-list-react/static/css/main.118af624.chunk.css"
+  },
+  {
+    "revision": "50a9c6cfe2903e5ff895",
+    "url": "/to-do-list-react/static/js/main.50a9c6cf.chunk.js"
+  },
+  {
+    "revision": "ceefb4fbb137ac8ec744",
+    "url": "/to-do-list-react/static/js/1.ceefb4fb.chunk.js"
+  },
+  {
+    "revision": "02cad368e11fe7d376b5",
+    "url": "/to-do-list-react/static/js/runtime~main.02cad368.js"
+  },
+  {
+    "revision": "c3a945933343291175528fbd900e08c8",
+    "url": "/to-do-list-react/index.html"
+  }
+];
